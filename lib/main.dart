155fc7112bfd2c2bb1d8be260a430a768c4eed02
unknown <nancykumari620@gmail.com>
2023-08-25 18:12:29 +0530

    Completed TodoApp for CodSoft Internship

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(),
       home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
